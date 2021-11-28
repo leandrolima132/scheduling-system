315817950217f456abc8835ca90fa98cd1829f49
Leandro Lima <leandrolima_ferreira@hotmail.com>
2021-11-28 01:55:35 -0300

    upgrade

--- src/Components/User/pages/Schedules.js
+++ src/Components/User/pages/Schedules.js
@@ -2,18 +2,17 @@ import React from "react";
 import api from "../../../api";
 
 const Schedules = ({ itens, isLoading, idAgendamento }) => {
-  console.log(idAgendamento.Agendamentos)
-  async function deletar() {
-    const id = localStorage.getItem('id')
-   
 
-    
-    // let confirmDelete = window.confirm('Deseja realmente excluir sua Pesquisa?')
-    // if(confirmDelete){
-      // await api.delete(`/auth/DeleteAgendamento/${id}/${itens._id}`)
+  async function deletar(e) {
+    const id = localStorage.getItem("id");
 
-    // }
-   }
+    let confirmDelete = window.confirm(
+      "Deseja realmente cancelar esse consulta?"
+    );
+    if (confirmDelete) {
+      await api.delete(`/auth/DeleteAgendamento/${id}/${e}`);
+    }
+  }
 
   return isLoading ? (
     <h1>Aguarde</h1>
@@ -42,7 +41,7 @@ const Schedules = ({ itens, isLoading, idAgendamento }) => {
                     border: "1px",
                     outline: "none",
                   }}
-                  onClick={deletar}
+                  onClick={() => deletar(item._id)}
                 >
                   Cancelar
                 </button>
@@ -55,49 +54,3 @@ const Schedules = ({ itens, isLoading, idAgendamento }) => {
 };
 
 export default Schedules;
-// state = {
-//   usuarios: [],
-// };
-
-// async componentDidMount() {
-//   const email = localStorage.getItem("email");
-//   const response = await api.get(`/auth/listAgendamentosByUser/${email}`);
-//   this.setState({ usuarios: response.data.Agendamentos.Agendamento });
-
-// }
-// render() {
-//   const { usuarios } = this.state;
-
-//   return (
-//     <div>
-//       <tr>
-//         <th>Data</th>
-//         <th>Retorno</th>
-//         <th>Descrição</th>
-//         <th>Fisioterapeuta</th>
-//       </tr>
-//       {usuarios !== 0 ?  usuarios.map((item) => (
-//         <tr key={item._id}>
-//           <td>{item.Data}</td>
-//           <td>{item.Retorno}</td>
-//           <td>{item.Descricao}</td>
-//           <td>{item.Medico}</td>
-//           <td>
-//             <button
-//               style={{
-//                 background: "red",
-//                 color: "white",
-//                 padding: "10px",
-//                 borderRadius: "5px",
-//                 border: "1px",
-//                 outline: "none",
-//               }}
-//             >
-//               Cancelar
-//             </button>
-//           </td>
-//         </tr>
-//       )) : ''}
-//     </div>
-//   );
-// }
